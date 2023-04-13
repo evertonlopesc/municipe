d57583815675faeffbc78ffecf8a5be03b7449bc
evertonlc <everton.lopess@outlook.com>
2023-04-13 16:42:31 -0300

    chore: add configuration for env from setup_system.

--- .setup_system.sh
+++ .setup_system.sh
@@ -4,9 +4,16 @@ echo -e "\e[31;43m***** Mount Project Image *****\e[0m"
 sudo chown -R $USER:USER .
 docker compose build
 echo ""
+echo -e "\e[31;43m***** Create env file *****\e[0m"
+touch ./.env
+echo "POSTGRES_HOST=db
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=postgres" >> ./.env
+echo ""
 echo -e "\e[31;43m***** Container Up *****\e[0m"
 docker compose up -d
 echo ""
 echo -e "\e[31;43m ***** Create Database *****\e[0m"
+docker compose run web rails db:create
 docker compose run web rails db:setup
 echo ""
