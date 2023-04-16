1fed6774ecfab84a34126d5057490f78b88d478a
evertonlc <everton.lopess@outlook.com>
2023-04-16 13:22:46 -0300

    chore: change title color from setup_system.

--- .setup_system.sh
+++ .setup_system.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-echo -e "\e[31;43m***** Mount Project Image *****\e[0m"
+echo -e "\e[97;100m***** Mount Project Image *****\e[0m"
 sudo chown -R $USER:$USER .
 docker compose build web
 echo ""
-echo -e "\e[31;43m***** Create env file *****\e[0m"
+echo -e "\e[97;100m***** Create env file *****\e[0m"
 touch ./.env
 echo "POSTGRES_HOST=db
 POSTGRES_USER=postgres
 POSTGRES_PASSWORD=postgres" >> ./.env
 echo ""
-echo -e "\e[31;43m***** Container Up *****\e[0m"
+echo -e "\e[97;100m***** Container Up *****\e[0m"
 docker compose up -d
 docker ps
 echo ""
-echo -e "\e[31;43m ***** Create Database *****\e[0m"
+echo -e "\e[97;100m ***** Create Database *****\e[0m"
 docker compose run web rails db:create
 docker compose run web rails db:setup
 echo ""
-echo -e "\e[31;43m ***** Run tests *****\e[0m"
+echo -e "\e[97;100m ***** Run tests *****\e[0m"
 docker compose run web rspec
 echo ""
