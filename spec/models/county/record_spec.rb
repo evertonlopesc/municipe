23c9f3884c4232050398799d0e7bada86880dafa
evertonlc <everton.lopess@outlook.com>
2023-04-20 16:37:24 -0300

    feat: add test to county with rspec.

--- spec/models/county/record_spec.rb
+++ spec/models/county/record_spec.rb
@@ -2,12 +2,34 @@ require 'rails_helper'
 
 RSpec.describe County::Record, type: :model do
   describe "instances" do
+    subject(:county) { FactoryBot.build(:county) }
+
     context "when build a county" do
       it "return valid" do
-        county = FactoryBot.build(:county)
-
         expect(county).to be_valid
       end
+
+      it "return active status" do
+        expect(county.active?).to be true
+      end
+    end
+  end
+
+  describe "validations" do
+    subject(:county) { FactoryBot.build(:county) }
+
+    context "when require presence" do
+      it { should validate_presence_of(:id_ibge) }
+      it { should validate_presence_of(:name) }
+      it { should validate_presence_of(:state) }
+    end
+
+    context "when require numericality" do
+      it { should validate_numericality_of(:id_ibge) }
+    end
+
+    context "when require length" do
+      it { should validate_length_of(:id_ibge).is_equal_to(7) }
     end
   end
 end
