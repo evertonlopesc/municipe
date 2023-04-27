a61efc0127c8c2a6c386448cfff83e03cca0d59c
evertonlc <everton.lopess@outlook.com>
2023-04-27 20:41:26 -0300

    feat: generate counties views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root "counties#index"
+  resources :counties
   resources :people
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  # root "articles#index"
 end
